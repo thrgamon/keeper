cc74178ff861160028644d1f8576484303b475a5
Tom Gamon <tom@tomgamon.com>
2022-06-21 22:58:37 +1000

    Setup dokku

--- scripts/setup-dokku.sh
+++ scripts/setup-dokku.sh
@@ -3,5 +3,5 @@ set -Eeuo pipefail
 
 ssh dokku apps:create ${APP_NAME}
 git remote add dokku dokku@${DOKKU_REMOTE}:${APP_NAME}
-ssh dokku domains:add ${APP_NAME}.${DOMAIN_NAME}
+ssh dokku domains:add ${APP_NAME} ${APP_NAME}.${DOMAIN_NAME}
 ssh dokku letsencrypt:enable ${APP_NAME}
